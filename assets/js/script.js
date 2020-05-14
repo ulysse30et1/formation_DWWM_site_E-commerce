8de3ee7fbd3277093045aa1c4c3635be107ecc52
amaral kevyn <amaral.kevyn@protonmail.com>
2020-05-14 15:41:53 +0200

    footer

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+let footer = document.querySelector('footer').innerHTML = (`
+<footer class="page-footer font-small blue pt-4 bg-secondary text-white">
+  <div class="container-fluid text-center text-md-left">
+    <div class="row">
+
+    <div class="col-12 mt-md-0 mt-3 text-center">
+        <h6 class="text-uppercase">Carte</h6>
+
+        <div class="map mb-5">
+        <p class="mb-2"<i class="fas fa-home mr-1 mb-2"></i>12 rue Paul Cézanne, 80000 Amiens</p>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2568.729874146789!2d2.294551816021174!3d49.92264467940603!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e7869845e11ec5%3A0x84ca374719a5b955!2s12%20Rue%20Paul%20C%C3%A9zanne%2C%2080080%20Amiens!5e0!3m2!1sfr!2sfr!4v1589457832869!5m2!1sfr!2sfr" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        </div>
+        </div>
+
+    <div class="col-md-4 mt-md-0 mt-3 text-center">
+    <h6 class="text-uppercase">A propos de nous</h6>
+    </div>
+      
+      <hr class="clearfix w-100 d-md-none pb-3">
+      <div class="col-md-4 mb-md-0 mb-3 text-center">
+
+        <h6 class="text-uppercase">Contact</h6>
+
+        <ul class="list-unstyled">
+        
+        <li>
+          <i class="fas fa-envelope mr-2"></i>
+          <a href="mailto:smartSmile@gmail.com" class="list-footer text-white" target="_blank" >smartSmile@gmail.com</a>
+          </li>
+
+        <li>
+          <i class="fas fa-phone-square-alt mr-2"></i>
+          <a class="list-footer">06-66-48-98-14</a>
+          </li>
+
+        </ul>
+      </div>
+
+      <div class="col-md-4 mb-md-0 mb-3 text-center">
+        <h6 class="text-uppercase">Informations légales</h6>
+        <ul class="list-unstyled">
+          <li>
+            <a href="legals.html" class="list-footer text-white" target="_blank">Mentions Légales</a>
+          </li>
+          <li>
+            <a href="Condition de ventes.html" class="list-footer text-white" target="_blank">Nos Conditions de Vente</a>
+          </li>
+         
+        </ul>
+
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright text-center py-3">© 2020 Copyright:
+    <a href="https://mdbootstrap.com/" class="text-warning">By Ulysse Chazot, Gautier Chassang, Kevyn Amaral.</a>
+  </div>
+</footer>
+`)
