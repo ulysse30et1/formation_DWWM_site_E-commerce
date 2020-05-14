33d631ed6ed8d81accf0deef46879c12f5245660
ulysse <ulysse.chazot@gmail.com>
2020-05-14 14:20:27 +0200

    barre de nav

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+document.getElementById('smallScreen').innerHTML = (`
+<ul class="nav">
+<li class="nav-item">
+  <a class="nav-link" href="#">sportives</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">urbaines</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">hybrides</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">enfants</a>
+</li>
+</ul>
+`)
+
+document.getElementById('largeScreen').innerHTML = (`
+<ul class="nav flex-column">
+<li class="nav-item">
+  <a class="nav-link" href="#">sportives</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">urbaines</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">hybrides</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">enfants</a>
+</li>
+</ul>
+`)
